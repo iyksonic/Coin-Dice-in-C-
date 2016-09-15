0136b7c2dae83ec5d4ca38d7c7a4c5936249f7c5
Ikedieze <iyksonic2006@yahoo.com>
2016-09-15 11:09:44 +0100

    dice

---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a;
+            Random generator = new Random();
+            a = (float)generator.NextDouble();
+           
+
+            if (a > 0.5f)
+            {
+                Console.WriteLine("Head");
+            }
+            else
+            {
+                Console.WriteLine("Tail");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
